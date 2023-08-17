b650da67268b9d36ff3748e167ed91d417a1593e
rama <ramahardianstudio@gmail.com>
2023-08-17 18:36:57 +0700

    'l'

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;700;900&amp;display=swap" />
+   
         <link rel="shortcut icon" type="image/png" href="/image/fav.png" />
         <link rel="stylesheet" href="/css/bootstrap.css" />
         <link rel="stylesheet" href="/css/fontawesome.css" />
