72ab4d47752e0a66c9d9f53876d143636c801f28
rama <ramahardianstudio@gmail.com>
2023-08-17 10:13:36 +0700

    'l'

--- src/component/layout.js
+++ src/component/layout.js
@@ -1,4 +1,11 @@
 import React from 'react';
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = false
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
+export const maxDuration = 5
 export default class Layout extends React.Component {
   componentDidMount () {}
   render () {
