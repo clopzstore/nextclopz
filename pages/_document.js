87b8f3de71b794b3a4e697acf57df12a24cfa1b6
rama <ramahardianstudio@gmail.com>
2023-08-17 18:33:01 +0700

    'l'

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;700;900&amp;display=swap"
-        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;700;900&amp;display=swap" />
         <link rel="shortcut icon" type="image/png" href="/image/fav.png" />
         <link rel="stylesheet" href="/css/bootstrap.css" />
         <link rel="stylesheet" href="/css/fontawesome.css" />
