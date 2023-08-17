3c46c5843002ff2b17cfa34c9188dba68d3bfc30
rama <ramahardianstudio@gmail.com>
2023-08-17 09:48:03 +0700

    'l'

--- src/component/layout.js
+++ src/component/layout.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+export const runtime = 'edge';
 export default class Layout extends React.Component {
   componentDidMount () {}
   render () {
