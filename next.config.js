e3aaa01a694b6c7500136dea3c6f0c27d5ad0a7a
rama <ramahardianstudio@gmail.com>
2023-08-18 09:36:55 +0700

    'l'

--- next.config.js
+++ next.config.js
@@ -3,18 +3,7 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false, 
   reactStrictMode: true,  
-  images: { 
-  domains: ["v5.airtableusercontent.com"], 
-  formats: ['image/avif', 'image/webp'],
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'v5.airtableusercontent.com',
-      port: '',
-      pathname: '',
-    },
-  ],
-},
+  output: "standalone",
   experimental: {
     runtime: 'experimental-edge',
   },
